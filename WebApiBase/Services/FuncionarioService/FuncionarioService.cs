039f9e45d8e8e8b2904db2c5bc13b25b8d9aac21
Leandro Lima <lleandrorj@gmail.com>
2024-03-30 19:15:53 -0300

    Implementing all Interface methods in the FuncionarioService class

--- WebApiBase/Services/FuncionarioService/FuncionarioService.cs
+++ WebApiBase/Services/FuncionarioService/FuncionarioService.cs
@@ -1,10 +1,42 @@
-﻿using WebApiBase.Models;
+﻿using WebApiBase.DataContext;
+using WebApiBase.Models;
 
 namespace WebApiBase.Services.FuncionarioService
 {
     public class FuncionarioService : IFuncionarioInterface
     {
+        private readonly AplicationDbContext _context;
+        public FuncionarioService(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> CreateFuncionario(FuncionarioModel novoFuncionario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> DeleteFuncionario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<FuncionarioModel>> GetFuncionarioById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<ServiceResponse<List<FuncionarioModel>>> GetFuncionarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> InactiveFuncionario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<FuncionarioModel>>> UpdateFuncionario(FuncionarioModel editatoFuncionario)
         {
             throw new NotImplementedException();
         }
